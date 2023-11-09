e295531dbd0114a0b360a64ce588644eb2f2ecd8
Muhammad Ikram Sabila Rasyad <ikramsabila@gmail.com>
2023-11-09 16:44:44 +0700

    y

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
     <section class="awal" id="awal">
-        
+        <div class="container">
+            <div class="judul">
+                <h3 class="Hawal">Welcome to Brawivent</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis eligendi esse a autem ipsum aut
+                    nisi,
+                    aperiam officia ab natus, magnam deserunt repudiandae?</p>
+                <button class="button">Know More</button>
+            </div>
+        </div>
     </section>
 </body>
+
 </html>
